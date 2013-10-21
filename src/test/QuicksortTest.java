67e1fdb15323cacdfa32fa752a5af2fdd533312a
Shervin Aflatooni <shervinaflatooni@gmail.com>
2013-10-21 20:17:43 +1100

    ADded best test so far for quick sort

--- src/test/QuicksortTest.java
+++ src/test/QuicksortTest.java
@@ -10,25 +10,40 @@ public class QuicksortTest {
 
     /**
      * 
+     * 100% line coverage, 77% mutation coverage
      */
-    @Test
-    public void test1() {
+    //@Test
+    /**public void test1() {
         int []init = {}; 
         int []expected = {};
         Quicksort qs = new Quicksort();
         qs.sort(init);
         
         assertArrayEquals("when the array is size one", expected, init); 
-    }
+    }**/
     
-    @Test
-    public void test2() {
+    //@Test
+    /**public void test2() {
         int []init = {7,9,8,10}; 
         int []expected = {7,8,9,10};
         Quicksort qs = new Quicksort();
         qs.sort(init);
         
         assertArrayEquals("when the array is size one", expected, init); 
+    }**/
+    
+    
+    //===============================================================
+    /**
+     * 96% coverage, 88% mutation coverage (should kill all non-equ, still need to check though)
+     */
+    @Test
+    public void test3() {
+        int []init = {7,9,8,10,1}; 
+        int []expected = {1,7,8,9,10};
+        Quicksort qs = new Quicksort();
+        qs.sort(init);
+        
+        assertArrayEquals("when the array is size one", expected, init); 
     }
-
 }
