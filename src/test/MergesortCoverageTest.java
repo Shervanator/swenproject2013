1dcb4a4a2285da175bfc70ac730885321364b741
Shervin Aflatooni <shervinaflatooni@gmail.com>
2013-10-23 19:38:32 +1100

    Add merge sort tests

---
+++ src/test/MergesortCoverageTest.java
@@ -0,0 +1,24 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import sort.Bubblesort;
+import sort.Mergesort;
+
+
+public class MergesortCoverageTest {
+	/**
+	 * 100% line coverage test, only 82% mutation score
+	 */
+	@Test
+	public void merge_sort_coverage1() {
+		int []init = {2, 1, 9}; 
+		int []expected = {1, 2, 9};
+		Mergesort sort = new Mergesort();
+		sort.sort(init);
+		
+		assertArrayEquals("when the array is size one", expected, init); 
+	}
+}
---
+++ src/test/MergesortMutationTest.java
@@ -0,0 +1,34 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import sort.Bubblesort;
+import sort.Mergesort;
+
+
+public class MergesortMutationTest {
+	/**
+	 * 100% line coverage test, only 96% mutation score (full coverage)
+	 */
+	@Test
+	public void merge_sort_mutation1() {
+		int []init =     {2, 1, 9, 5, 2, 7, 1, 3, 5, 8, 2}; 
+		int []expected = {1, 1, 2, 2, 2, 3, 5, 5, 7, 8, 9};
+		Mergesort sort = new Mergesort();
+		sort.sort(init);
+		
+		assertArrayEquals("when the array is size one", expected, init); 
+	}
+	
+	@Test
+    public void merge_sort_mutation2() {
+        int []init =     {5, 4, 3, 2, 1}; 
+        int []expected = {1, 2, 3, 4, 5};
+        Mergesort sort = new Mergesort();
+        sort.sort(init);
+        
+        assertArrayEquals("when the array is size one", expected, init); 
+    }
+}
