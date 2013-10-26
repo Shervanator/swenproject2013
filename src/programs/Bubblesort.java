5f0d14fc2a34a8a63d88e3fb503dcab237f8eb76
zli7 <362413868@qq.com>
2013-10-27 03:09:41 +1100

    finish 20 faults in each file

--- src/programs/Bubblesort.java
+++ src/programs/Bubblesort.java
@@ -8,14 +8,14 @@ public class Bubblesort {
 	  int n, c, d;
 	  n = array.length;
 	  for (c = 0; c < ( n - 1 ); c++) {
-		for (d = 1; d < n - c - 1; d++) {
+		for (d = 0; d < n - c - 1; d++) {
 			if (array[d] > array[d+1]) /* For descending order use < */
 			{
-				swap(array, d, d+1);break;
+				swap(array, d, d+1);
 			}
 		}
-	  }
 	  
+	  }
 	  return array;
   }
 
